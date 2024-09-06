8bb0ca02b4446b7bc7cd019bbf4c79002f3c570f
pranavgnn <pranavgnayak@gmail.com>
2024-09-07 00:33:47 +0530

    viewBox

--- svg.py
+++ svg.py
@@ -6,12 +6,12 @@ padding = 1 # px
 def get_box_svg(i, j, value):
     return f"""
     <rect
-        id = \"{i}, {j}\"
-        width = {box_size}
-        height = {box_size}
-        rx = 2.5
-        transform = "translate({i * (box_size + padding)}, {j * (box_size + padding)})"
-        fill = "{"#161b22" if value == 0 else "#26a641"}"
+        id="{i}, {j}"
+        width={box_size}
+        height={box_size}
+        rx=2.5
+        transform="translate({i * (box_size + padding)}, {j * (box_size + padding)})"
+        fill="{"#161b22" if value == 0 else "#26a641"}"
     />
     """
 
@@ -31,20 +31,21 @@ def get(username):
 
             svg_str += get_box_svg(i, j, value)
 
+    svg_width = m * box_size + (m - 1) * padding
+    svg_height = n * box_size + (n - 1) * padding
+
     svg_str = f"""
 <svg
-    width = "{m * box_size + (m - 1) * padding}"
-    height = "{n * box_size + (n - 1) * padding}"
-    fill = "none"
-    xmlns = "http://www.w3.org/2000/svg"
-    role = "img"
-    aria-labelledby = "descId">
+    width="{svg_width}"
+    height="{svg_height}"
+    viewBox="0 0 {svg_width} {svg_height}"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    style="isolation: isolate"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    aria-labelledby="descId">
 >
-    <style>
-        rect {{
-            shape-renderer: crispEdges;
-        }}
-    </style>
     """ + svg_str + "\n</svg>"
         
     return svg_str
